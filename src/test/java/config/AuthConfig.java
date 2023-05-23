f76706ead986a03e99d55a9912771b802e3a1c8e
Irina <iparakhina91@gmail.com>
2023-05-24 00:09:58 +0300

    changed test

--- src/test/java/config/AuthConfig.java
+++ src/test/java/config/AuthConfig.java
@@ -2,14 +2,35 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+//@Config.Sources({
+       // "classpath:${env}.properties"
+//})
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "classpath:${env}.properties"
+        "system:properties",
+        "classpath:auth.properties"
 })
 
 public interface AuthConfig extends Config {
-    @Config.Key("username.selenoid")
+   // @Config.Key("username.selenoid")
+   // String remote_username();
+
+   // @Config.Key("password.selenoid")
+    //String remote_password();
+
+    @Key("username.selenoid")
     String remote_username();
 
-    @Config.Key("password.selenoid")
+    @Key("password.selenoid")
     String remote_password();
+
+    @Key("apiToken")
+    String apiToken();
+
+    @Key("usernameAllure")
+    String usernameAllure();
+
+    @Key("passwordAllure")
+    String passwordAllure();
 }
